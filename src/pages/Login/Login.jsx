4f0b25d5b837afc6b235ffdbac35370f01289e82
mahdee18 <mahdeec10@gmail.com>
2025-11-22 02:38:52 +0600

    feat: build static UI for login and registration pages

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,5 @@
+
+const LoginPage = () => {
+  return <div className="text-red-500">Login Page</div>;
+};
+export default LoginPage;
---
+++ src/pages/Registration/Registration.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Registration = () => {
+  return (
+    <div>
+        <h2 className="text-3xl text-blue-600 font-bold text-center mt-10">Welcome to the Registration Page</h2>
+    </div>
+  );
+};
+
+export default Registration;
+
